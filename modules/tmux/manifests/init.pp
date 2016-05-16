23d5eb534ea3b11a41924f0f6fe0f76df6ddd0bd
Aurelien Vallee <vallee.aurelien@gmail.com>
2016-05-16 01:09:16 +0000

    Install tmux alongside tmux.conf

--- modules/tmux/manifests/init.pp
+++ modules/tmux/manifests/init.pp
@@ -1,8 +1,13 @@
 class tmux {
 
+    package { 'tmux':
+        ensure => present,
+    }
+
     file { "${::home}/.tmux.conf":
-        ensure => file,
-        source => 'puppet:///modules/tmux/tmux.conf'
+        ensure  => file,
+        source  => 'puppet:///modules/tmux/tmux.conf',
+        require => Package['tmux'],
     }
 
 }
