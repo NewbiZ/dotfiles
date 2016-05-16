0ec610832ed0ebc2f629b00c7670e0a3bf1c6de5
avallee <vallee.aurelien@gmail.com>
2016-05-16 23:17:45 +0000

    Add conditional run of linter and doc generation

--- apply.sh
+++ apply.sh
@@ -3,33 +3,50 @@
 # Current script directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Check for lint errors
-echo -e "\n> Checking lint:"
-puppet-lint --no-documentation-check ${DIR}
-if [ $? -eq 0 ]
+# Function to check if a program is available
+function check_program() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+if check_program puppet-lint
 then
-    echo "Everything perfect."
+    # Check for lint errors
+    echo -e "\n> Checking lint:"
+    puppet-lint --no-documentation-check ${DIR}
+    if [ $? -eq 0 ]
+    then
+        echo "Everything perfect."
+    else
+        echo "Aborting."
+        exit 1
+    fi
+fi
+
+if check_program puppet
+then
+    # Puppet modules
+    echo -e "\n> Updating puppet modules:"
+    sudo puppet module install puppetlabs-vcsrepo
+
+    # Apply puppet manifest locally
+    echo -e "\n> Applying puppet manifest:"
+    sudo puppet apply ${DIR}/manifests/site.pp \
+        --hiera_config ${DIR}/hiera.yaml \
+        --modulepath ${DIR}/modules \
+        --graph \
+        --graphdir ${DIR}/doc
 else
-    echo "Aborting."
+    echo "Puppet client not found. Aborting."
     exit 1
 fi
 
-# Puppet modules
-echo -e "\n> Updating puppet modules:"
-sudo puppet module install puppetlabs-vcsrepo
-
-# Apply puppet manifest locally
-echo -e "\n> Applying puppet manifest:"
-sudo puppet apply ${DIR}/manifests/site.pp \
-    --hiera_config ${DIR}/hiera.yaml \
-    --modulepath ${DIR}/modules \
-    --graph \
-    --graphdir ${DIR}/doc
-
-# Generate documentation
-echo -e "\n> Generating documentation in doc/"
-UNPRIVILEGED_USER=$USER
-sudo chown -R $UNPRIVILEGED_USER doc/
-dot -Tpng doc/expanded_relationships.dot -o doc/expanded_relationships.png
-dot -Tpng doc/relationships.dot -o doc/relationships.png
-dot -Tpng doc/resources.dot -o doc/resources.png
+if check_program dot
+then
+    # Generate documentation
+    echo -e "\n> Generating documentation in doc/"
+    UNPRIVILEGED_USER=$USER
+    sudo chown -R $UNPRIVILEGED_USER doc/
+    dot -Tpng doc/expanded_relationships.dot -o doc/expanded_relationships.png
+    dot -Tpng doc/relationships.dot -o doc/relationships.png
+    dot -Tpng doc/resources.dot -o doc/resources.png
+fi
