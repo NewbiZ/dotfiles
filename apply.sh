672e22384a36bee28db55840d75a1c94de02b530
Aurelien Vallee <vallee.aurelien@gmail.com>
2016-05-16 00:57:54 +0000

    Improve apply.sh logging

--- apply.sh
+++ apply.sh
@@ -4,13 +4,20 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Check for lint errors
-echo "> Checking lint:"
+echo -e "\n> Checking lint:"
 puppet-lint --no-documentation-check ${DIR}
+if [ $? -eq 0 ]
+then
+    echo "Everything perfect."
+else
+    echo "Aborting."
+    exit 1
+fi
 
 # Puppet modules
-echo "> Updating puppet modules:"
+echo -e "\n> Updating puppet modules:"
 sudo puppet module install puppetlabs-vcsrepo
 
 # Apply puppet manifest locally
-echo "> Applying puppet manifest:"
+echo -e "\n> Applying puppet manifest:"
 puppet apply --modulepath ${DIR}/modules ${DIR}/manifests/site.pp
